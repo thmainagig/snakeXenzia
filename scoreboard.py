1bb0c6455fb7b380b872062fe22dfea9fedab0a2
thmainagig <thbenson4@gmail.com>
2023-05-17 20:18:40 +0300

    Commit with high score saved

--- scoreboard.py
+++ scoreboard.py
@@ -5,7 +5,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score=0
+        with open('data.txt') as f:
+            self.high_score = int(f.read())
         self.color("white")
         self.penup()
         self.goto(x=-30, y=270)
@@ -27,6 +28,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', 'w') as f:
+                f.write(f"{self.high_score}")
         self.score=0
         self.update_scoreboard()
 
